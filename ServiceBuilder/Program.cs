f7775d157a0de6e678ee0809a9b26fa92cb16659
Formbi <minnisiat@gmail.com>
2020-07-24 19:18:07 +0400

    Initial commit

---
+++ ServiceBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
